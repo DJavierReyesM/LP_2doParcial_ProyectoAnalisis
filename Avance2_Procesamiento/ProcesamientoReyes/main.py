c2e7ce745bb399bc14b00b7f3d8a8579804e0772
Diego Javier Reyes Medranda <diejarey@espol.edu.ec>
2023-01-30 14:31:16 -0500

    Ajuste avance preprocesamiento/visualizacion - Diego Reyes

--- Avance2_Procesamiento/ProcesamientoReyes/main.py
+++ Avance2_Procesamiento/ProcesamientoReyes/main.py
@@ -3,8 +3,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib import rc
 
+# archivo con nombre de dataset generado en el scraping
 archivo = "dataset_dreyes.csv"
 
 #Lectura del Dataset
@@ -12,12 +12,12 @@ def leer_archivo(nombre_archivo):
     df = pd.read_csv(nombre_archivo)
     return df
 
-# Función para visualización 1: Top 10 usuarios con más medallas de oro
+# Función para visualización 1: Top 10 usuarios de StackOverflow con más medallas de oro
 def visual1(dataset):
     # Dataset ordenado en base a la cantidad de medallas de oro
     ordered_df = dataset.sort_values(by='N_oro', ascending=False)
     ordered_df.reset_index(inplace=True)
-    print("Dataset ordenado por medallas de oro")
+    print("\nDataset ordenado por medallas de oro")
     print(ordered_df.to_string())
     # Dataset con Top 10 usuarios con medallas de oro
     medallas10 = ordered_df[:10][::-1]
@@ -30,7 +30,7 @@ def visual1(dataset):
     plt.figure(figsize=(13, 6))
     plt.hlines(y=my_range, xmin=0, xmax=medallas10['N_oro'], color='skyblue')
     plt.plot(medallas10['N_oro'], my_range, "o", alpha=0.4, zorder=0)
-    # Se añaden titulos
+    # Se añaden titulos y etiquetas
     plt.yticks(my_range, medallas10['Nombre'])
     plt.title("Top 10 usuarios de StackOverflow con más medallas de Oro", loc='center')
     plt.xlabel('Cantidad de medallas de Oro')
@@ -40,16 +40,17 @@ def visual1(dataset):
                 fontsize=9)
     plt.show()
 
+# Función para visualización 2: Top 10 usuarios de StackOverflow con más medallas de plata (sin info. de país/ubicación proporcionada y con reputación <= 350000)
 def visual2(dataset):
     # Se aplican los filtros de "país" = "Not provided by the user" y "reputación" <= 350000
     df = dataset[(dataset["Pais"]=="Not provided by the user") & (dataset["Reputacion"]<=350000)]
     # Se ordena el dataset en función a las medallas de plata
     ordered_df = df.sort_values(by='N_plata', ascending=False)
     ordered_df.reset_index(inplace=True)
-    print("Dataset: Ordenado por medallas de plata tras la aplicación de los filtros anteriores: (StacKOverflow)")
+    print("\nDataset: Ordenado por medallas de plata tras la aplicación de los pais y reputación")
     print(ordered_df.to_string())
     medallasplata = ordered_df[:10]
-    print("\nDataset con Top 10 usuarios con más medallas de plata (sin info. de ubicación proporcionada y con reputación <= 350000)")
+    print("\nDataset con Top 10 usuarios con más medallas de plata (sin info. de país/ubicación proporcionada y con reputación <= 350000)")
     print(medallasplata.to_string())
     # Se añaden valores a 2 listas, users y medals, para construir la gráfica
     users = []
@@ -66,24 +67,32 @@ def visual2(dataset):
     # Se recorren las filas del dataset de medallasplata para añadir los valores de las medallas dentro de la gráfica
     for row in medallasplata.itertuples():
         plt.text(row.Index, row.N_plata, row.N_plata,horizontalalignment='center', verticalalignment='bottom',fontsize=10)
-    plt.title("Top 10 usuarios con más medallas de plata (sin info. de ubicación proporcionada y con reputación <= 350000)", loc="left")
+    plt.title("Top 10 usuarios de StackOverflow con más medallas de plata (sin info. de país/ubicación proporcionada y con reputación <= 350000)", loc="left")
     plt.xlabel("Usuarios")
     plt.ylabel("# de Medallas de Plata")
     plt.show()
 
+# Función para visualización 3: Top 10 usuarios de StackOverflow procedentes de Alemania con más medallas (Oro, Plata y Bronce)
 def visual3(dataset):
+    # Se recorre el dataset para crear una nueva columna para este último: la suma total de las medallas
     total_medallas = []
     for i in range(len(dataset)):
         sumaMedallas = dataset.iloc[i]["N_oro"] + dataset.iloc[i]["N_plata"] + dataset.iloc[i]["N_bronce"]
         total_medallas.append(sumaMedallas)
+    # Se añade la columna al dataset
     dataset["totalMedallas"] = total_medallas
 
-    expresion2 = ".*Germany.*"
+    # Se filtra el dataset por el país/ubicación con coincidencias de Germany o Deutschland usando una expresión regular
+    expresion2 = "(.*Germany.*|.*Deutschland.*)"
     dataset = dataset[(dataset.Pais.str.match(expresion2))]
+    print("\nDataset de usuarios procedentes de Alemania")
+    print(dataset.to_string())
     ordered_df = dataset.sort_values(by='totalMedallas', ascending=False)[:10][::-1]
     ordered_df.reset_index(inplace=True)
+    print("\nDataset ordenado por total de medallas")
     print(ordered_df[::-1].to_string())
-
+    # Se crean listas para guardar los valores de las medallas y los nombres de usuario que representarán los valores
+    # de las barras en el gráfico
     bars1 = []
     bars2 = []
     bars3 = []
@@ -93,15 +102,18 @@ def visual3(dataset):
         bars2.append(ordered_df.iloc[i]["N_plata"])
         bars3.append(ordered_df.iloc[i]["N_oro"])
         names.append(ordered_df.iloc[i]["Nombre"])
-    # Se construye el gráfico
+    # Se construye el gráfico usando las listas creadas
+    # "bars" representa el acumulado entre las medallas de bronce y plata para poder posicionar correctamente
+    # las barras en el gráfico
     bars = np.add(bars1, bars2).tolist()
     plt.figure(figsize=(13.3, 6.3))
     b1 = plt.barh(names, bars1, color="#e1c77b")
     b2 = plt.barh(names, bars2, left=bars1, color="#cac5b4")
     b3 = plt.barh(names, bars3, left= bars, color="#ecc349")
-
-    plt.legend([b3, b2, b1], ["Oro", "Plata", "Bronce" ], title="Medallas", loc="lower right")
-    plt.title("Top 10 usuarios de StackOverflow de Alemania con más medallas (Oro,Plata, Bronce)", loc='center')
+    # Se añaden las leyendas en el gráfico y se recorre el dataset filtrado para poner anotar los valores de las medallas
+    # en el gráfico
+    plt.legend([b3, b2, b1], ["Oro", "Plata", "Bronce"], title="Medallas", loc="lower right")
+    plt.title("Top 10 usuarios de StackOverflow procedentes de Alemania con más medallas (Oro, Plata y Bronce)", loc='center')
     for i in range(len(ordered_df)):
         plt.text((ordered_df.iloc[i]["N_bronce"])/2,i - 0.2, ordered_df.iloc[i]["N_bronce"], horizontalalignment='center', verticalalignment='bottom',
                 fontsize=9)
@@ -112,15 +124,25 @@ def visual3(dataset):
                  horizontalalignment='center',
                  verticalalignment='bottom',
                  fontsize=9)
+        plt.text((ordered_df.iloc[i]["N_bronce"] + ordered_df.iloc[i]["N_plata"] + ordered_df.iloc[i]["N_oro"])+70,
+                 i - 0.2, "= "+ str(ordered_df.iloc[i]["totalMedallas"]),
+                 horizontalalignment='center',
+                 verticalalignment='bottom',
+                 fontsize=9)
     # Se muestra el gráfico
+    plt.xlabel("Cantidad total de medallas")
     plt.show()
 
+# Presentación de gráficas:
 dataset = leer_archivo(archivo)
 #Gráfica 1
+print("Gráfica 1")
 visual1(dataset)
 
 #Gráfica 2
+print("\nGráfica 2")
 visual2(dataset)
 
 #Gráfica 3
+print("\nGráfica 3")
 visual3(dataset)
